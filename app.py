e1c0e33e2ccb3d1680b418a11c53581e86e3690d
Kirtan <105850020+Kirtan-Mudaliyar@users.noreply.github.com>
2025-07-18 18:43:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,8 +69,8 @@ def set_custom_theme():
 set_custom_theme()
 
 
-st.title("Fabric Defect Detection with YOLOv8")
-st.markdown("The Fabric Defect Detector is a real-time AI-based web app built using YOLOv8 to identify fabric defects like holes, tears, and stains from images or live webcam input. Trained on the AITEX dataset and developed using Streamlit, it offers a fast and user-friendly solution for textile quality inspection.")
+st.title(" Fab Watch – Real-Time AI Fabric Defect Detection System")
+st.markdown("The Fabric Defect Detector is a real-time AI-based web app built using YOLOv8 to identify fabric defects like holes, pinch, broken stitch and stains from images or live webcam input. Trained on the "Multi-Class Fabric Defect Detection Dataset" dataset and developed using Streamlit, it offers a fast and user-friendly solution for textile quality inspection.")
 
 # === 📌 Sidebar Info ===
 with st.sidebar:
